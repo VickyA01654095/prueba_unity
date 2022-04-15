226c9e5cec11c9c351a129d79218e39775421d78
Victoria Vazquez <65380353+VickyA01654095@users.noreply.github.com>
2022-04-15 17:51:00 -0500

    Ya funciona xd

--- unityAzure/conexiones/urls.py
+++ unityAzure/conexiones/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('alta', views.alta, name='alta'),
     path('consulta', views.consulta, name='consulta'),
+    path('consulta_usuario', views.consulta_usuario, name='consulta_usuario'),
     path('cambio', views.cambio, name='cambio'),
 ]
--- unityAzure/conexiones/views.py
+++ unityAzure/conexiones/views.py
@@ -10,13 +10,102 @@ import sqlite3
 # Create your views here.
 
 
+@csrf_exempt
 def alta(request):
-    pass
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+
+    username = body['username']
+    comment = body['comment']
+
+    mydb = sqlite3.connect("tarea_U_DJ.db")
+    cur = mydb.cursor()
+
+    stringSQL = '''INSERT INTO Usuario (username, comment)
+VALUES (?, ?)'''
+
+    rows = cur.execute(stringSQL, (username, comment,))
+    mydb.commit()
+
+    if rows is None:
+        raise Http404("It was not possible to register that user")
+    else:
+        d = {"msg": "User added with success!"}
+        j = dumps(d)
+
+    mydb.close()
+    return HttpResponse(j, content_type="text/json-comment-filtered")
 
 
 def consulta(request):
-    pass
+    mydb = sqlite3.connect("tarea_U_DJ.db")
+    cur = mydb.cursor()
+    stringSQL = '''SELECT id as user_id, username, comment FROM Usuario'''
+    rows = cur.execute(stringSQL)
+    if rows is None:
+        raise Http404("List unavailable")
+    else:
+        lista_salida = []
+        for r in rows:
+            print(r)
+            d = {}
+            d["user_id"] = r[0]
+            d["username"] = r[1]
+            d["comment"] = r[2]
+            lista_salida.append(d)
+        j = dumps(lista_salida)
+    return HttpResponse(j, content_type="text/json-comment-filtered")
 
 
+@csrf_exempt
+def consulta_usuario(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+
+    user_id = body['id']
+
+    mydb = sqlite3.connect("tarea_U_DJ.db")
+    cur = mydb.cursor()
+    stringSQL = '''SELECT id as user_id, username, comment FROM Usuario WHERE user_id = ?'''
+    rows = cur.execute(stringSQL, (user_id,))
+    if rows is None:
+        raise Http404("List unavailable")
+    else:
+        lista_salida = []
+        for r in rows:
+            print(r)
+            d = {}
+            d["user_id"] = r[0]
+            d["username"] = r[1]
+            d["comment"] = r[2]
+            lista_salida.append(d)
+        j = dumps(lista_salida)
+    return HttpResponse(j, content_type="text/json-comment-filtered")
+
+
+@csrf_exempt
 def cambio(request):
-    pass
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+
+    user_id = body['id']
+    username = body['username']
+    comment = body['comment']
+
+    mydb = sqlite3.connect("tarea_U_DJ.db")
+    cur = mydb.cursor()
+
+    stringSQL = '''UPDATE Usuario SET username = ?, comment = ?
+    WHERE Usuario.id = ?'''
+
+    rows = cur.execute(stringSQL, (username, comment, user_id))
+    mydb.commit()
+
+    if rows is None:
+        raise Http404("It was not possible to update that user")
+    else:
+        d = {"msg": "Data updated with success!"}
+        j = dumps(d)
+
+    mydb.close()
+    return HttpResponse(j, content_type="text/json-comment-filtered")
